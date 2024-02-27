cd36d36dc94acb778390bae55d8ebfd95be8fcae
harshit27rajput <harshit002.hk@gmail.com>
2024-02-27 18:44:15 +0530

    Signin, Signup completed

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,11 +1,15 @@
 const { User } = require('../models/user.model.js')
 const bcryptjs =require( 'bcryptjs');
+const errorHandler=require('../utils/error.js')
+const jwt=require('jsonwebtoken')
+
 const signup=async(req, res,next)=>{
     console.log(req.body)
     const {username,email,password}=req.body;
 
     if(!username || !email || !password || username===''|| email===""|| password===""){
-        return res.status(400).json({msg:"Please fill out all fields"})
+        return next(errorHandler(400,'All fields are required'));
+        // return res.status(400).json({msg:"Please fill out all fields"})
     }
     const hashedPassword= bcryptjs.hashSync(password, 10);
     const newUser= new User ({
@@ -19,5 +23,31 @@ const signup=async(req, res,next)=>{
         next(error)
     }
 }
+const signin=async(req, res, next)=>{
+    const{email, password}=req.body
+    if(!email || !password || email==='' || password===''){
+        next(errorHandler(400,"Email and Password is required"))
+    }
+    try{
+    const isValidUser=await User.findOne({email});
+    if(!isValidUser){
+        return next(errorHandler(401, 'Invalid Email or Password'))
+    }
+    const isValidPassword=bcryptjs.compareSync(password, isValidUser.password)
+    if (!isValidPassword){
+        return next(errorHandler(400,'Invalid Password'))
+    }
+    const token=jwt.sign({id:isValidUser._id},process.env.JWT_SECURE)
+    const {password:pass, ...others}= isValidUser._doc
 
-module.exports ={signup}
+    res.status(200)
+    .cookie('access_token',token,{
+        httpOnly:true
+    })
+    .json(others)
+}
+catch(error){
+    next(error)
+}
+}
+module.exports ={signup,signin}
