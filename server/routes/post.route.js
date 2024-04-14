1c1b246fd0beec75ed47bad53b8ff8ba79b73fba
harshit27rajput <harshit002.hk@gmail.com>
2024-04-14 17:10:58 +0530

    created get posts api route

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const {verifyToken}=require('../utils/verifyUser.js')
-const {create} = require('../controllers/post.controller.js')
+const {create, getposts} = require('../controllers/post.controller.js')
 
 const router= express.Router();
 router.post('/create', verifyToken, create)
+router.post('/getposts', getposts)
+
 export default router;
